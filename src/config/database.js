7203203b0a51a5cffb265af08af37e332a4ddd01
PASUMARTHI BALU <131604638+Balu2200@users.noreply.github.com>
2024-12-22 17:39:12 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose")
 const connectDb = async() =>{
     
     try{
-        await mongoose.connect("mongodb+srv://BaluPasumarthi:BaluPasumarthi22@firstdb.7argj.mongodb.net/Dev_Connect");
+        await mongoose.connect("mongodb+srv://BaluPasumarthi:*****@firstdb.7argj.mongodb.net/Dev_Connect");
     }
     catch(err){
         console.error("Error in connection", err.message);
